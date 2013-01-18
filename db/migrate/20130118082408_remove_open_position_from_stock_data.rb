d97df70b79d9fb43a0aa4a5c4191de7a2175c271
Andy Watts <andywatts@yahoo.com>
2013-01-18 19:42:51 +0800

    remove unneeded migratin

--- db/migrate/20130118082408_remove_open_position_from_stock_data.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOpenPositionFromStockData < ActiveRecord::Migration
-  def up
-     remove_column :stock_dates, :open_position,           :string
-  end
-
-  def down
-     add_column :stock_dates, :open_position,           :string
-  end
-end
