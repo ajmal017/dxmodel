728ea4ced6b15b8413546d04c560cbc1254f2ab2
Andy Watts <andywatts@yahoo.com>
2013-01-20 00:57:04 +0800

    added charts based on bloomberg

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,6 +3,17 @@ class StocksController < ApplicationController
   def show
     @stock = Stock.find params[:id]
     @stock_dates = @stock.stock_dates.order("date desc")
-  end
 
+    begin
+      url = "http://www.bloomberg.com/markets/chart/data/1Y/#{@stock.ticker}:#{@stock.country}"
+      require 'open-uri'
+      json_string = open(url).read
+      parsed_json = ActiveSupport::JSON.decode(json_string)
+
+      @stock_close_prices = parsed_json['data_values']
+    
+    rescue
+    end
+
+  end
 end
