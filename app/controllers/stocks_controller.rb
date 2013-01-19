261d807f2b7239c2da1115cc87449a4994437817
Andy Watts <andywatts@yahoo.com>
2013-01-20 01:08:55 +0800

    Added charts to stock and trade show pages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,16 +4,5 @@ class StocksController < ApplicationController
     @stock = Stock.find params[:id]
     @stock_dates = @stock.stock_dates.order("date desc")
 
-    begin
-      url = "http://www.bloomberg.com/markets/chart/data/1Y/#{@stock.ticker}:#{@stock.country}"
-      require 'open-uri'
-      json_string = open(url).read
-      parsed_json = ActiveSupport::JSON.decode(json_string)
-
-      @stock_close_prices = parsed_json['data_values']
-    
-    rescue
-    end
-
   end
 end
