51aaf21438ef5fa480df6f14156a86807c1226fa
Andy Watts <andywatts@yahoo.com>
2013-04-19 23:13:54 +0800

    change apac to ap

--- config/deploy/production_apac.rb
+++
@@ -1,6 +0,0 @@
-set :rails_env, "production_apac"
-set :application, "dxmodel_apac"
-role :web, "dxmodel_apac.andywatts.com"
-role :app, "dxmodel_apac.andywatts.com"
-role :db,  "dxmodel_apac.andywatts.com", :primary => true
-
--- config/deploy/production_eu.rb
+++ config/deploy/production_eu.rb
@@ -1,1 +1,5 @@
 set :rails_env, "production_eu"
+set :euplication, "dxmodel_eu"
+role :web, "dxmodel_eu.andywatts.com"
+role :eup, "dxmodel_eu.andywatts.com"
+role :db,  "dxmodel_eu.andywatts.com", :primary => true
