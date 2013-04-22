fbd48ed2d9ffe2a380a9d3ad444a084e15974ef1
Andy Watts <andywatts@yahoo.com>
2013-04-23 07:57:45 +0800

    fix big numbers

---
+++ db/migrate/20130422235011_fix_big_numbers.rb
@@ -0,0 +1,23 @@
+class FixBigNumbers < ActiveRecord::Migration
+  def change
+     change_column :stock_dates, :alpha, :decimal, :default => 0.0, :precision => 14, :scale => 6
+
+     change_column :stock_dates, :per, :decimal, :default => 0.0, :precision => 14, :scale => 6
+     change_column :stock_dates, :per_change, :decimal, :default => 0.0, :precision => 14, :scale => 6
+
+     change_column :stock_dates, :pfcf, :decimal, :default => 0.0, :precision => 14, :scale => 6
+     change_column :stock_dates, :pfcf_change, :decimal, :default => 0.0, :precision => 14, :scale => 6
+
+     change_column :stock_dates, :eps_5yr_growth, :decimal, :default => 0.0, :precision => 14, :scale => 6
+
+     change_column :stock_dates, :roe_bf12m, :decimal, :default => 0.0, :precision => 14, :scale => 6
+     change_column :stock_dates, :roa_bf12m, :decimal, :default => 0.0, :precision => 14, :scale => 6
+
+     change_column :stock_dates, :average_traded_value_30_days, :decimal, :default => 0.0, :precision => 20, :scale => 6
+
+     change_column :stock_dates, :close, :decimal, :default => 0.0, :precision => 14, :scale => 6
+
+     change_column :stock_dates, :wmavg_10d, :decimal, :default => 0.0, :precision => 14, :scale => 6
+     change_column :stock_dates, :smavg_10d, :decimal, :default => 0.0, :precision => 14, :scale => 6
+  end
+end
