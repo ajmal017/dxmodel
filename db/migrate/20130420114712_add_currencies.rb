0917ea6d1f6000a09e1133387b433ff7b2aac6a4
Andy Watts <andywatts@yahoo.com>
2013-04-22 14:48:59 +0800

    remove unnecessary migration

--- db/migrate/20130420114712_add_currencies.rb
+++ db/migrate/20130420114712_add_currencies.rb
@@ -2,5 +2,6 @@ class AddCurrencies < ActiveRecord::Migration
   def change
      add_column :fx_rates, :usdgbp, :decimal, :precision => 14, :scale => 6
      add_column :fx_rates, :usdeur, :decimal, :precision => 14, :scale => 6
+     add_column :fx_rates, :usdjpy, :decimal, :precision => 14, :scale => 6
   end
 end
