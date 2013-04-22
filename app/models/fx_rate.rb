5354f52d0fa9bec00ae69f491701454dfd569f17
Andy Watts <andywatts@yahoo.com>
2013-04-22 13:17:51 +0800

    fx validation

--- app/models/fx_rate.rb
+++ app/models/fx_rate.rb
@@ -2,10 +2,13 @@ class FxRate < ActiveRecord::Base
   attr_accessible :date, :usdsgd, :usdhkd, :usdcny
 
   validates :date, :presence => true
-#  validates :usdsgd, :presence => true, :uniqueness => {:scope => :date}
-#  validates :usdhkd, :presence => true, :uniqueness => {:scope => :date}
-#  validates :usdgbp, :presence => true, :uniqueness => {:scope => :date}
-#  validates :usdeur, :presence => true, :uniqueness => {:scope => :date}
+  validates :usdsgd, :presence => true, :uniqueness => {:scope => :date}, :if => Proc.new {|a| CURRENCIES.include?('SGD') }
+  validates :usdhkd, :presence => true, :uniqueness => {:scope => :date}, :if => Proc.new {|a| CURRENCIES.include?('HKD') }
+  validates :usdcny, :presence => true, :uniqueness => {:scope => :date}, :if => Proc.new {|a| CURRENCIES.include?('CNY') }
+  validates :usdjpy, :presence => true, :uniqueness => {:scope => :date}, :if => Proc.new {|a| CURRENCIES.include?('JPY') }
+  validates :usdgbp, :presence => true, :uniqueness => {:scope => :date}, :if => Proc.new {|a| CURRENCIES.include?('GBP') }
+  validates :usdeur, :presence => true, :uniqueness => {:scope => :date}, :if => Proc.new {|a| CURRENCIES.include?('EUR') }
+
 
   default_scope order('fx_rates.date DESC')
 
