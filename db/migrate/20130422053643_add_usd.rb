4a5e379fe2175c1aa2e06cb49e7d48ec918915df
Andy Watts <andywatts@yahoo.com>
2013-04-22 14:44:05 +0800

    fix enter local price column

--- db/migrate/20130422053643_add_usd.rb
+++ db/migrate/20130422053643_add_usd.rb
@@ -1,5 +1,5 @@
 class AddUsd < ActiveRecord::Migration
   def change
-     add_column :fx_rates, :usdjpy, :decimal, :precision => 14, :scale => 6
+     add_column :fx_rates, :usdusd, :decimal, :precision => 14, :scale => 6
   end
 end
