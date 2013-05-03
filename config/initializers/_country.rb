3a497d0806083b10180f1e6364b2a1a083c32b7a
Andy Watts <andywatts@yahoo.com>
2013-05-04 01:14:50 +0800

    fundamental screen variables

--- config/initializers/_country.rb
+++ config/initializers/_country.rb
@@ -2,3 +2,7 @@ COUNTRY = "AP"
 INDEX_TICKER = 'HSI'
 INDEX_NAME = 'Hong Kong Hang Seng Index'
 CURRENCIES = ["USD", "SGD", "HKD", "CNY"]
+
+# Fundamentals
+ENTER_RANK_THRESHOLD = 20
+EXIT_RANK_THRESHOLD = 30
