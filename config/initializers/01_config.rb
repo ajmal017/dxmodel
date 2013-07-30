ac83bd767e412fc9ed3797b35c32c6242d08b451
Andy Watts <andywatts@yahoo.com>
2013-07-30 10:16:23 +0800

    oz config

--- config/initializers/01_config.rb
+++ config/initializers/01_config.rb
@@ -3,14 +3,14 @@ MAX_PER_ENTRY = 1000000
 MAX_NUMBER_OF_STOCKS = 30  # E.g. 30 long AND 30 short
 
 
-COUNTRY = "US"
-CURRENCIES = ["USD"]
+COUNTRY = "AU"
+CURRENCIES = ["ASD", "NZD", "USD"]
 
 
 LONG_ENTER_RANK_THRESHOLD  = 20
-LONG_EXIT_RANK_THRESHOLD   = 75
+LONG_EXIT_RANK_THRESHOLD   = 30
 SHORT_ENTER_RANK_THRESHOLD = 20
-SHORT_EXIT_RANK_THRESHOLD  = 75
+SHORT_EXIT_RANK_THRESHOLD  = 30
 
 
 MA = true
@@ -20,10 +20,10 @@ MA_SHORT_ENTER = true  # Use MA signal to enter short?
 MA_SHORT_EXIT  = false # Use MA signal to exit short?
 
 
-RSI = true
+RSI = false
 RSI_OVERBOUGHT  = 70.0
 RSI_OVERSOLD    = 35.0
-RSI_LONG_ENTER  = true  # Use RSI signal to enter long?
+RSI_LONG_ENTER  = false  # Use RSI signal to enter long?
 RSI_LONG_EXIT   = false # Use RSI signal to exit long?
-RSI_SHORT_ENTER = true  # Use RSI signal to enter long?
+RSI_SHORT_ENTER = false  # Use RSI signal to enter long?
 RSI_SHORT_EXIT  = false # Use RSI signal to exit short?
