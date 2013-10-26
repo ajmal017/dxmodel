2cca099c73fe236376c661eaf3b9aca5e6676d93
Andy Watts <andywatts@yahoo.com>
2013-10-26 18:02:28 +0800

    fix

--- config/initializers/01_config.rb
+++ config/initializers/01_config.rb
@@ -7,14 +7,6 @@ COUNTRY = "JP"
 CURRENCIES = ["JPY", "USD"]
 INDEX_TICKER = "NKY"
 INDEX_NAME = "Nikkei 225"
-=======
-
-COUNTRY = "AP"
-INDEX_TICKER = 'HSI'
-INDEX_NAME = 'Hong Kong Hang Seng Index'
-CURRENCIES = ["USD", "SGD", "HKD", "CNY"]
->>>>>>> master
-
 
 # Fundamentals
 LONG_ENTER_RANK_THRESHOLD  = 20
@@ -32,18 +24,9 @@ MA_SHORT_EXIT  = false # Use MA signal to exit short?
 
 
 RSI = false
-<<<<<<< HEAD
 RSI_OVERBOUGHT  = 70.0
 RSI_OVERSOLD    = 35.0
 RSI_LONG_ENTER  = true  # Use RSI signal to enter long?
 RSI_LONG_EXIT   = false # Use RSI signal to exit long?
 RSI_SHORT_ENTER = true  # Use RSI signal to enter long?
 RSI_SHORT_EXIT  = false # Use RSI signal to exit short?
-=======
-#RSI_OVERBOUGHT  = 70.0
-#RSI_OVERSOLD    = 35.0
-#RSI_LONG_ENTER  = true  # Use RSI signal to enter long?
-#RSI_LONG_EXIT   = false # Use RSI signal to exit long?
-#RSI_SHORT_ENTER = true  # Use RSI signal to enter long?
-#RSI_SHORT_EXIT  = false # Use RSI signal to exit short?
->>>>>>> master
