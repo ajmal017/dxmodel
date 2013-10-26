3b0feb680d4d3ea1ea688cf89a4418c76e5c2016
Andy Watts <andywatts@yahoo.com>
2013-10-26 17:54:00 +0800

    fix

--- config/initializers/01_config.rb
+++ config/initializers/01_config.rb
@@ -2,7 +2,6 @@ AUM = 20000000.0
 MAX_PER_ENTRY = 1000000
 MAX_NUMBER_OF_STOCKS = 30  # E.g. 30 long AND 30 short
 
-<<<<<<< HEAD
 COUNTRY = "UK"
 CURRENCIES = ["GBp","EUR"]
 INDEX_TICKER = 'UKX'
@@ -13,21 +12,6 @@ LONG_ENTER_RANK_THRESHOLD = 20
 LONG_EXIT_RANK_THRESHOLD = 30
 SHORT_ENTER_RANK_THRESHOLD = 20
 SHORT_EXIT_RANK_THRESHOLD  = 30
-=======
-
-COUNTRY = "AP"
-INDEX_TICKER = 'HSI'
-INDEX_NAME = 'Hong Kong Hang Seng Index'
-CURRENCIES = ["USD", "SGD", "HKD", "CNY"]
-
-
-# Fundamentals
-LONG_ENTER_RANK_THRESHOLD  = 20
-LONG_EXIT_RANK_THRESHOLD   = 30
-SHORT_ENTER_RANK_THRESHOLD = 20
-SHORT_EXIT_RANK_THRESHOLD  = 30
-
->>>>>>> master
 
 #Technicals
 MA = true
@@ -38,23 +22,9 @@ MA_SHORT_EXIT = true # Use MA signal to exit short?
 
 
 RSI = false
-<<<<<<< HEAD
 RSI_OVERBOUGHT = 70.0
 RSI_OVERSOLD = 35.0
 RSI_LONG_ENTER = true  # Use RSI signal to enter long?
 RSI_LONG_EXIT = false  # Use RSI signal to exit long?
 RSI_SHORT_ENTER = true  # Use RSI signal to enter long?
 RSI_SHORT_EXIT = false  # Use RSI signal to exit short?
-
-
-
-
-
-=======
-#RSI_OVERBOUGHT  = 70.0
-#RSI_OVERSOLD    = 35.0
-#RSI_LONG_ENTER  = true  # Use RSI signal to enter long?
-#RSI_LONG_EXIT   = false # Use RSI signal to exit long?
-#RSI_SHORT_ENTER = true  # Use RSI signal to enter long?
-#RSI_SHORT_EXIT  = false # Use RSI signal to exit short?
->>>>>>> master
