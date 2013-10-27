e2db09e3ce4b898f276623eb0e73c4cda998f991
Andy Watts <andywatts@yahoo.com>
2013-10-27 10:21:04 +0800

    merge

--- config/initializers/01_config.rb
+++ config/initializers/01_config.rb
@@ -3,19 +3,12 @@ MAX_PER_ENTRY = 1000000
 MAX_NUMBER_OF_STOCKS = 30  # E.g. 30 long AND 30 short
 
 
-<<<<<<< HEAD
 COUNTRY = "AU"
 CURRENCIES = ["AUD", "NZD", "USD"]
 
 # Index
 INDEX_TICKER = "AS51"
 INDEX_NAME = "ASX 200"
-=======
-COUNTRY = "AP"
-INDEX_TICKER = 'HSI'
-INDEX_NAME = 'Hong Kong Hang Seng Index'
-CURRENCIES = ["USD", "SGD", "HKD", "CNY"]
->>>>>>> master
 
 
 # Fundamentals
@@ -34,18 +27,9 @@ MA_SHORT_EXIT  = false # Use MA signal to exit short?
 
 
 RSI = false
-<<<<<<< HEAD
 RSI_OVERBOUGHT  = 70.0
 RSI_OVERSOLD    = 35.0
 RSI_LONG_ENTER  = false  # Use RSI signal to enter long?
 RSI_LONG_EXIT   = false # Use RSI signal to exit long?
 RSI_SHORT_ENTER = false  # Use RSI signal to enter long?
 RSI_SHORT_EXIT  = false # Use RSI signal to exit short?
-=======
-#RSI_OVERBOUGHT  = 70.0
-#RSI_OVERSOLD    = 35.0
-#RSI_LONG_ENTER  = true  # Use RSI signal to enter long?
-#RSI_LONG_EXIT   = false # Use RSI signal to exit long?
-#RSI_SHORT_ENTER = true  # Use RSI signal to enter long?
-#RSI_SHORT_EXIT  = false # Use RSI signal to exit short?
->>>>>>> master
