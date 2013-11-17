bbebbc97b151eb65d81fd6ab63ced1dd6f9e1bcb
Andy Watts <andywatts@yahoo.com>
2013-11-17 18:01:17 +0800

    validation

--- app/models/stock_date.rb
+++ app/models/stock_date.rb
@@ -6,13 +6,13 @@ class StockDate < ActiveRecord::Base
   validates :stock_id, :presence => true, :uniqueness => {:scope => :date}
 
 # Should validate for the below, but currently the upload controller builds and saves stock_date records in pieces...
-#  validates :date, :presence => true
-#  validates :long_fund_score, :presence => true
-#  validates :long_fund_rank_by_industry, :presence => true
-#  validates :long_fund_rank, :presence => true
-#  validates :short_fund_score, :presence => true
-#  validates :short_fund_rank_by_industry, :presence => true
-#  validates :short_fund_rank, :presence => true
+  validates :date, :presence => true
+  validates :long_fund_score, :presence => true
+  validates :long_fund_rank_by_industry, :presence => true
+  validates :long_fund_rank, :presence => true
+  validates :short_fund_score, :presence => true
+  validates :short_fund_rank_by_industry, :presence => true
+  validates :short_fund_rank, :presence => true
 #  validates :alpha, :presence => true
 #  validates :per, :presence => true
 #  validates :per_change, :presence => true
@@ -24,9 +24,9 @@ class StockDate < ActiveRecord::Base
 #  validates :roe_bf12m, :presence => true
 #  validates :roa_bf12m, :presence => true
 #  validates :average_traded_value_30_days, :presence => true
-#  validates :close, :presence => true
-#  validates :wmavg_10d, :presence => true
-#  validates :smavg_10d, :presence => true
+  validates :close, :presence => true
+  validates :wmavg_10d, :presence => true
+  validates :smavg_10d, :presence => true
 
 
 
