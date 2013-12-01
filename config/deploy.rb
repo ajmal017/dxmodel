d68a3427dd1579574bdc0afcce5196c3e937ab8e
Andy Watts <andywatts@yahoo.com>
2013-12-01 21:01:53 +0800

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,18 +10,15 @@ set :format, :pretty
 set :log_level, :info 
 set :pty, true
 set :keep_releases, 5
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :migration_rol, 'db'
 
 # Roles
 role :app, %w{dxmodel@dxmodel.com}
 role :web, %w{dxmodel@dxmodel.com}
 role :db,  %w{dxmodel@dxmodel.com}
-
-# Servers
-server 'dxmodel.com', user: 'dxmodel', roles: [:web, :app]
-
 
 namespace :deploy do
 
